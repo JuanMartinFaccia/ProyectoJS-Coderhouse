0ddf6c5f63c9c06020185305017be86f3b94ba38
Juan Martín Faccia <martin.faccia@outlook.com>
2023-02-07 16:18:42 -0300

    subo stock.json

--- js/index.js
+++ js/index.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', pintarProductos(productos))
 
 document.addEventListener('DOMContentLoaded', cargarCarrito) 
 
+//JSON
 
+
+
+const lista = document.getElementById('producto-contenedor') 
+
+fetch('/data.json')
+   .then((resp) => resp.json())
+   .then(data => console.log(data))
 
 
 
